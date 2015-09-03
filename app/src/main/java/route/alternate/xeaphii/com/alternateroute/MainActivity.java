6e5973cbf52dff99638c56a0a654364ad80b061d
Xeaphii <muhammadzeeshan020@gmail.com>
2015-09-03 05:49:29 +0500

    Screen 1 layout

--- app/src/main/java/route/alternate/xeaphii/com/alternateroute/MainActivity.java
+++ app/src/main/java/route/alternate/xeaphii/com/alternateroute/MainActivity.java
@@ -2,16 +2,19 @@ package route.alternate.xeaphii.com.alternateroute;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,9 +23,11 @@ import java.util.Locale;
 
 public class MainActivity extends Activity {
 
-    AutoCompleteTextView ClassesSelect,SourceEt,DestinationET;
-    EditText DateSelected,TrainNumber;
+    AutoCompleteTextView SourceEt,DestinationET;
+    Button DateSelected;
+    EditText TrainNumber;
     Button BtSubmit;
+    Button ClassesSelect;
 
 
     String[] ClassesList={"SL","3AC","2AC","1AC","CC","FC","3E","2S"};
@@ -32,28 +37,57 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ClassesSelect=(AutoCompleteTextView)findViewById(R.id.classes_select);
+        ClassesSelect=(Button)findViewById(R.id.classes_set);
         SourceEt=(AutoCompleteTextView)findViewById(R.id.source_field);
         DestinationET=(AutoCompleteTextView)findViewById(R.id.destination_field);
-        DateSelected= (EditText) findViewById(R.id.date_selected);
+        DateSelected= (Button) findViewById(R.id.date_set);
         TrainNumber = (EditText) findViewById(R.id.train_number);
 
         BtSubmit = (Button) findViewById(R.id.bt_submit);
 
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,ClassesList);
+        //ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,ClassesList);
+        ClassesSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.select_train_class_dialog);
+                dialog.setTitle("Train Classes available");
 
+                // set the custom dialog components - text, image and button
+                ListView SelectClasses = (ListView) dialog.findViewById(R.id.train_classes_lov);
+                SelectClasses.setItemsCanFocus(true);
+                SelectClasses.setAdapter(new TrainClassesAdapter(MainActivity.this, 0,ClassesList));
+                SelectClasses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        ClassesSelect.setText(ClassesList[position]);
+                        dialog.dismiss();
+                    }
+                });
+//                Button bt_submit = (Button) dialog.findViewById(R.id.bt_submit);
+//                bt_submit.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Intent i = new Intent(MainActivity.this, PercentagesCovered.class);
+//                        startActivity(i);
+//                        dialog.dismiss();
+//                    }
+//                });
+                dialog.show();
+            }
+        });
         ArrayAdapter StationLovAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.list_of_stations));
         SourceEt.setAdapter(StationLovAdapter);
         DestinationET.setAdapter(StationLovAdapter);
-        ClassesSelect.setAdapter(adapter);
+        //ClassesSelect.setAdapter(adapter);
         BtSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(TrainNumber.getText().toString().trim().equals("")){
-                    Intent i = new Intent(MainActivity.this,TrainsList.class);
+                if (TrainNumber.getText().toString().trim().equals("")) {
+                    Intent i = new Intent(MainActivity.this, TrainsList.class);
                     startActivity(i);
-                }else{
-                    Intent i = new Intent(MainActivity.this,PercentagesCovered.class);
+                } else {
+                    Intent i = new Intent(MainActivity.this, PercentagesCovered.class);
                     startActivity(i);
                 }
             }
@@ -83,16 +117,16 @@ public class MainActivity extends Activity {
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
-        DateSelected.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    new DatePickerDialog(MainActivity.this, date, myCalendar
-                            .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                            myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-                }
-            }
-        });
+//        DateSelected.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                if (hasFocus) {
+//                    new DatePickerDialog(MainActivity.this, date, myCalendar
+//                            .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+//                            myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+//                }
+//            }
+//        });
                 //ClassesList.setThreshold(1);
     }
 
